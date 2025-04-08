628963366376b392d0bcca73c0b920c79a09970f
edwardrl101 <edward.rl101@gmail.com>
2025-04-08 12:07:29 +0800

    Improve test method names

--- src/test/java/fintrek/parser/FileDataParserTest.java
+++ src/test/java/fintrek/parser/FileDataParserTest.java
@@ -16,56 +16,56 @@ public class FileDataParserTest {
     }
 
     @Test
-    void parse_validBudgetLine_success() {
+    void parseValidBudgetLine_success() {
         String line = "Monthly Budget: $2000.50";
         ParseResult<Void> result = FileDataParser.parseFileData(line);
         assertTrue(result.isSuccess());
     }
 
     @Test
-    void parse_invalidBudgetLine_failure() {
+    void parseInvalidBudgetLine_failure() {
         String line = "Monthly Budget: $abc";
         ParseResult<Void> result = FileDataParser.parseFileData(line);
         assertFalse(result.isSuccess());
     }
 
     @Test
-    void parse_invalidCategoryLine_failure() {
+    void parseInvalidCategoryLine_failure() {
         String repeatedLetters = "a".repeat(101);
         ParseResult<Void> result = FileDataParser.parseFileData(repeatedLetters);
         assertFalse(result.isSuccess());
     }
 
     @Test
-    void parse_validExpenseLine_success() {
+    void parseValidExpenseLine_success() {
         String line = "Lunch | $12.50 | FOOD | 01-04-2024";
         ParseResult<Void> result = FileDataParser.parseFileData(line);
         assertTrue(result.isSuccess());
     }
 
     @Test
-    void parse_validRecurringExpenseLine_success() {
+    void parseValidRecurringExpenseLine_success() {
         String line = "Netflix | $15.00 | ENTERTAINMENT | 01-03-2024 | R";
         ParseResult<Void> result = FileDataParser.parseFileData(line);
         assertTrue(result.isSuccess());
     }
 
     @Test
-    void parse_expenseLine_missingDate_failure() {
+    void parseExpenseLineMissingDate_failure() {
         String line = "Lunch | $12.50 | FOOD";
         ParseResult<Void> result = FileDataParser.parseFileData(line);
         assertFalse(result.isSuccess());
     }
 
     @Test
-    void parse_expenseLine_invalidAmount_failure() {
+    void parseExpenseLineInvalidAmount_failure() {
         String line = "Groceries | $abc | FOOD | 01-01-2024";
         ParseResult<Void> result = FileDataParser.parseFileData(line);
         assertFalse(result.isSuccess());
     }
 
     @Test
-    void parse_expenseLine_invalidDate_failure() {
+    void parseExpenseLineInvalidDate_failure() {
         String line = "Taxi | $8.00 | TRANSPORT | 2024-01-01";
         ParseResult<Void> result = FileDataParser.parseFileData(line);
         assertFalse(result.isSuccess());
